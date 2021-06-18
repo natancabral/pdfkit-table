1424fbfe622c864b9edc6acc2df0f77f7f3048ee
natancabral <natancabral@gnial.com.br>
2021-06-18 18:20:01 -0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,197 @@
 # pdfkit-table
+
+#### Generate pdf tables with javascript (PDFKIT plugin)
+Helps to draw informations in simple tables using pdfkit. #server-side.
+
+
+## Examples
+
+[view pdf example](https://github.com/natancabral/pdfkit-table/raw/main/example/document.pdf) | 
+[full code example](https://github.com/natancabral/pdfkit-table/blob/main/example/index-server-example.js) |
+[both](https://github.com/natancabral/pdfkit-table/blob/main/example/)
+
+<img src="https://github.com/natancabral/pdfkit-table/blob/main/example/pdf-sample.png"/>
+
+### Start
+
+```bash
+npm install pdfkit-table
+```
+
+```js
+  const fs = require("fs");
+  const PDFDocument = require("pdfkit-table");
+  const doc = new PDFDocument({ margin: 30, size: 'A4' });
+
+  // the magic:
+  doc.table( table, options );
+  //...
+
+```
+
+### Example 1 - Simple Array Table
+```js
+  // requires 
+  const tableArray = {
+    headers: ["Country", "Conversion rate", "Trend"],
+    rows: [
+      ["Switzerland", "12%", "+1.12%"],
+      ["France", "67%", "-0.98%"],
+      ["England", "33%", "+4.44%"],
+    ],
+  };
+  doc.moveDown().table( tableArray, { width: 300 }); // A4 595.28 x 841.89 (portrait) (about width sizes)
+  // end code
+```
+
+
+### Example 2 - Table
+```js
+  // require
+  // A4 595.28 x 841.89 (portrait) (about width sizes)
+  const table = {
+    headers: [
+      { label:"Name", property: 'name', width: 60, renderer: null },
+      { label:"Description", property: 'description', width: 150, renderer: null }, 
+      { label:"Price 1", property: 'price1', width: 100, renderer: null }, 
+      { label:"Price 2", property: 'price2', width: 100, renderer: null }, 
+      { label:"Price 3", property: 'price3', width: 100, renderer: null }, 
+      { label:"Price 4", property: 'price4', width: 43, renderer: null },
+    ],
+    datas: [
+      {description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis ante in laoreet egestas. ', price1: '$1', price3: '$ 3', price2: '$2', price4: '$4',name: 'Name 1', },
+      {name: 'bold:Name 2', description: 'bold:Lorem ipsum dolor.', price1: 'bold:$1', price3: '$3', price2: '$2', price4: '$4', options: { fontSize: 8, separation: true}},
+      {name: 'Name 3', description: 'Lorem ipsum dolor.', price1: 'bold:$1', price4: '$4', price2: '$2', price3: {label:'PRICE $3', options: { fontSize: 12 }}, },
+    ],
+    rows: [
+      [
+        "Apple",
+        "Nullam ut facilisis mi. Nunc dignissim ex ac vulputate facilisis.",
+        "$ 105,99",
+        "$ 105,99",
+        "$ 105,99",
+        "$ 105,99",
+      ],
+      [
+        "Tire",
+        "Donec ac tincidunt nisi, sit amet tincidunt mauris. Fusce venenatis tristique quam, nec rhoncus eros volutpat nec. Donec fringilla ut lorem vitae maximus. Morbi ex erat, luctus eu nulla sit amet, facilisis porttitor mi.",
+        "$ 105,99",
+        "$ 105,99",
+        "$ 105,99",
+        "$ 105,99",
+      ],
+    ],
+  };
+
+  doc.table(table, {
+    prepareHeader: () => doc.font("Helvetica-Bold").fontSize(8),
+    prepareRow: (row, i) => doc.font("Helvetica").fontSize(8),
+  });
+```
+
+
+### Example 3 - Full Code
+```js
+  // require
+  const fs = require("fs");
+  const PDFDocument = require("pdfkit-table");
+  const doc = new PDFDocument({
+    margin: 30, 
+  });
+
+  doc.pipe(fs.createWriteStream("./file-table.pdf"));
+
+  // table code
+
+  doc.pipe(res); // HTTP response only to show pdf
+  doc.end();
+```
+
+## Table
+ 
+Example code:
+```js
+const table = {
+  // simple headers only with ROWS (not DATAS)  
+  headers: ['Name', 'Age'],
+  // simple content
+  rows: [
+    ['Jack', '32'], // row 1
+    ['Maria', '30'], // row 2
+  ]
+};
+
+const table = {
+  // complex headers work with ROWS and DATAS  
+  headers: [
+    { label:"Name", property: 'name', width: 100, renderer: null },
+    { label:"Age", property: 'age', width: 100, renderer: null },
+  ],
+  // simple content (works fine!)
+  rows: [
+    ['Jack', '32'], // row 1
+    ['Maria', '30'], // row 2
+  ]
+  // complex content
+  datas: [
+    { name: 'Jack', age: 32, },
+    // age is object value with style options
+    { name: 'Maria', age: { label: 30 , options: { fontSize: 12 }}, },
+  ],
+};
+
+```
+
+## Options
+
+| *Properties*         | description       |
+-----------------------|-------------------|
+| **width**            | width of table    |
+| **x**                | position x (left) |
+| **y**                | position y (top)  |
+| **columnSpacing**    | 5 |
+| **rowSpacing**       | 3 |
+| **prepareHeader**    | Function |
+| **prepareRow**       | Function |
+
+
+Example code:
+```js
+const options = {
+  // properties
+  width: 500, // {Number} default: undefined // A4 595.28 x 841.89 (portrait) (about width sizes)
+  x: 500, // {Number} default: undefined
+  y: 500, // {Number} default: undefined
+  columnSpacing: 5, // {Number} default: 5
+  rowSpacing: 3, // {Number} default: 3
+  // functions
+  prepareHeader: () => doc.font("Helvetica-Bold").fontSize(8), // {Function} 
+  prepareRow: (row, i) => doc.font("Helvetica").fontSize(8), // {Function} 
+}
+```
+
+## License
+
+The MIT License.
+
+## Author
+
+<table>
+  <tr>
+    <td>
+      <img src="https://github.com/natancabral.png?s=100" width="100"/>
+    </td>
+    <td>
+      Natan Cabral<br />
+      <a href="mailto:natancabral@hotmail.com">natancabral@hotmail.com</a><br />
+      <a href="https://github.com/natancabral/">https://github.com/natancabral/</a>
+    </td>
+  </tr>
+</table>
+
+## Thank you
+
+- pdfkit - [pdfkit](https://www.npmjs.com/package/pdfkit)
+- code base - [andronio](https://www.andronio.me/2017/09/02/pdfkit-tables/)
+- ideas - [giuseppe-santoro](https://github.com/foliojs/pdfkit/issues/29#issuecomment-56504943)
+- influence [voilab](https://github.com/voilab/voilab-pdf-table)
---
+++ index.js
@@ -0,0 +1,312 @@
+"use strict";
+
+const PDFDocument = require("pdfkit");
+
+class PDFDocumentWithTables extends PDFDocument {
+  
+  constructor(options) {
+    super(options);
+  }
+
+  /**
+   * table
+   * @param {object} table 
+   * @param {object} options 
+   * @returns 
+   */
+  table(table, options) {
+
+    let startX = this.page.margins.left,
+      startY = this.y;
+    options || (options = {});
+
+    if( typeof options !== 'object' ) return;
+
+    options.hasOwnProperty('x') && (startX = options.x);
+    options.hasOwnProperty('y') && (startY = options.y);
+
+    const columnCount = table.headers.length;
+    const columnSpacing = options.columnSpacing || 5; // 15
+    const columnSizes = options.columnSizes || [];
+    const columnPositions = []; // 0,10,20,30,100 |  |  |  |     |
+    const rowSpacing = options.rowSpacing || 3; // 5
+    const usableWidth =
+      options.width ||
+      this.page.width - this.page.margins.left - this.page.margins.right;
+
+    const prepareHeader = options.prepareHeader || (() => this.font("Helvetica-Bold").fontSize(8));
+    const prepareRow = options.prepareRow || (() => this.font("Helvetica").fontSize(8) );
+    
+    const prepareRowOptions = ( row ) => {
+      if( typeof row !== 'object' || !row.hasOwnProperty('options') ) return; 
+      row.options.hasOwnProperty('fontFamily') && this.font(row.options.fontFamily); 
+      row.options.hasOwnProperty('fontSize') && this.fontSize(row.options.fontSize); 
+      row.options.hasOwnProperty('color') && this.fillColor(row.options.color); 
+    }
+    
+    const computeRowHeight = (row) => {
+      
+      let result = 0;
+     
+      // reconhece se é uma linha object, content with property
+      if( !Array.isArray(row) && typeof row === 'object' && !row.hasOwnProperty('property') ){
+        const cells = []; 
+        // get all properties names on header
+        table.headers.forEach(({property}) => cells.push(row[property]) );
+        // define row with properties header
+        row = cells;  
+      }
+
+      row.forEach((cell,i) => {
+
+        let text = cell;
+
+        // object
+        // read cell and get label of object
+        if( typeof cell === 'object' ){
+          // define label
+          text = String(cell.label);
+          // apply font size on calc about height row 
+          cell.hasOwnProperty('options') && prepareRowOptions(cell);
+        }
+
+        // calc
+        // calc height size of string
+        const cellHeight = this.heightOfString(text, {
+          // width: columnWidth,
+          width: columnSizes[i],
+          align: "left",
+        });
+        
+        result = Math.max(result, cellHeight);
+
+      });
+
+      return result + rowSpacing;
+    };
+
+    const columnContainerWidth = usableWidth / columnCount;
+    const columnWidth = columnContainerWidth - columnSpacing;
+    const maxY = this.page.height - this.page.margins.bottom;
+
+    let rowBottomY = 0;
+    // let tableMaxHeightY = 0;
+
+    this.on("pageAdded", () => {
+      startY = this.page.margins.top;
+      rowBottomY = 0;
+      // tableMaxHeightY = 0;
+    });
+
+    // Allow the user to override style for headers
+    prepareHeader();
+
+    // Check to have enough room for header and first rows. default 3
+    if (startY + 2 * computeRowHeight(table.headers) > maxY) this.addPage();
+
+    if(table.headers && table.headers.length > 0){
+
+      if(typeof table.headers[0] === 'string' ){
+
+        //Print all headers
+        table.headers.forEach((header, i) => {
+          const posX = startX + i * columnContainerWidth;
+          this.text(header, posX, startY, {
+            width: columnWidth,
+            align: "left",
+          });
+          columnSizes.push(columnWidth);
+          columnPositions.push(posX);
+        });
+
+      }else{
+
+        let rowHeight = computeRowHeight(table.headers);
+
+        // Print all headers
+        let lastPosition = startX;
+        table.headers.forEach(({label,width}, i) => {
+          //this.fillColor('red').strokeColor('#777777');
+          
+          this.rect(lastPosition, startY - 5, width - 1, rowHeight + 3)
+          .fillColor('grey')
+          .fillOpacity(.1)
+          .strokeColor('black')
+          .strokeOpacity(1)
+          .fill()
+          .stroke();
+
+          this.fillColor('black') // restore color
+          .fillOpacity(1)
+          .strokeOpacity(1);
+
+          this.text(label, lastPosition + 2, startY, {
+            width: width,
+            align: "left",
+          })
+          columnSizes.push(width);
+          columnPositions.push(lastPosition);
+          lastPosition += width;
+        });
+
+        if( table.headers.hasOwnProperty('options') ){
+          table.headers.options.hasOwnProperty('fontFamily') && this.font(table.headers.options.fontFamily); 
+          table.headers.options.hasOwnProperty('fontSize') && this.fontSize(table.headers.options.fontSize); 
+        }
+
+      }
+    }
+
+    // Refresh the y coordinate of the bottom of the headers row
+    rowBottomY = Math.max(startY + computeRowHeight(table.headers), rowBottomY);
+    // console.log('rowBottomY',rowBottomY,'startY',startY,'computeRowH', computeRowHeight(table.headers));
+
+    // Separation line between headers and rows
+    this.moveTo(startX, rowBottomY - rowSpacing * 0.5)
+      .lineTo(startX + usableWidth, rowBottomY - rowSpacing * 0.5)
+      .lineWidth(1)
+      .stroke();
+
+    // ------------------------------------------------------------------------------
+    // data -------------------------------------------------------------------------
+    // ------------------------------------------------------------------------------
+    table.datas || (table.datas = [])
+    table.datas.forEach((row, i) => {
+      const rowHeight = computeRowHeight(row);
+
+      // Switch to next page if we cannot go any further because the space is over.
+      // For safety, consider 3 rows margin instead of just one
+      if (startY + 2 * rowHeight < maxY) {
+        startY = rowBottomY + rowSpacing;
+      }
+      else {
+        this.addPage();
+      }
+
+      // Allow the user to override style for rows
+      prepareRow(row, i);
+      prepareRowOptions(row);
+
+      if( row.hasOwnProperty('options') ){
+        if( row.options.hasOwnProperty('separation') ){
+            // Separation line between rows
+            this.moveTo(startX, rowBottomY - rowSpacing * 0.5)
+              .lineTo(startX + usableWidth, rowBottomY - rowSpacing * 0.5)
+              .lineWidth(1)
+              .opacity(1)
+              .stroke();
+        }
+      }
+
+      let posX = startX; 
+      // Print all cells of the current row
+      table.headers.forEach(({property,width}) => {
+        // const posX = startX + i * columnContainerWidth;
+        // const posX = columnPositions[i];
+        // const cWidth = columnWidth;
+        // const cWidth = columnSizes[i];
+        let text = row[property];
+        let origText = row[property];
+
+        if(typeof text === 'object' ){
+          text = String(text.label);
+          origText = String(text.label);
+          row[property].hasOwnProperty('options') && prepareRowOptions(row[property]);
+        }
+        
+        // bold
+        if( text.indexOf('bold:') === 0 ){
+          this.font('Helvetica-Bold');
+          text = text.replace('bold:','');
+        }
+        // size
+        if( text.indexOf('size') === 0 ){
+          let size = String(text).substr(4,2).replace(':','').replace('+','') >> 0;
+          this.fontSize( size < 7 ? 7 : size );
+          text = text.replace(`size${size}:`,'');
+        }
+
+        this.text(text, posX, startY, {
+          width: width,
+          align: "left",
+        });
+        posX += width; 
+
+        // repare font family
+        // if( origText.indexOf('bold:') === 0 || origText.indexOf('size') === 0 ){
+          prepareRow(row, i);
+          prepareRowOptions(row);
+        // }
+
+      });
+
+      // Refresh the y coordinate of the bottom of this row
+      rowBottomY = Math.max(startY + rowHeight, rowBottomY);
+
+      // Separation line between rows
+      this.moveTo(startX, rowBottomY - rowSpacing * 0.5)
+        .lineTo(startX + usableWidth, rowBottomY - rowSpacing * 0.5)
+        .lineWidth(.5)
+        .opacity(.5)
+        .stroke()
+        .opacity(1); // Reset opacity after drawing the line
+    });
+    // ------------------------------------------------------------------------------
+    // end data ---------------------------------------------------------------------
+    // ------------------------------------------------------------------------------
+
+    // ------------------------------------------------------------------------------
+    // rows -------------------------------------------------------------------------
+    // ------------------------------------------------------------------------------
+    table.rows || (table.rows = [])
+    table.rows.forEach((row, i) => {
+      const rowHeight = computeRowHeight(row);
+
+      // Switch to next page if we cannot go any further because the space is over.
+      // For safety, consider 3 rows margin instead of just one
+      if (startY + 2 * rowHeight < maxY) {
+        startY = rowBottomY + rowSpacing;
+      }
+      else {
+        this.addPage();
+      }
+
+      // Allow the user to override style for rows
+      prepareRow(row, i);
+
+      // Print all cells of the current row
+      row.forEach((cell, i) => {
+        // const posX = startX + i * columnContainerWidth;
+        const posX = columnPositions[i];
+        // const cWidth = columnWidth;
+        const cWidth = columnSizes[i];
+        this.text(cell, posX, startY, {
+          width: cWidth,
+          align: "left",
+        });
+
+        // tableMaxHeightY = this.y > tableMaxHeightY ? this.y : tableMaxHeightY; 
+        // console.log(this.y,tableMaxHeightY);
+      });
+
+      // Refresh the y coordinate of the bottom of this row
+      rowBottomY = Math.max(startY + rowHeight, rowBottomY);
+
+      // Separation line between rows
+      this.moveTo(startX, rowBottomY - rowSpacing * 0.5)
+        .lineTo(startX + usableWidth, rowBottomY - rowSpacing * 0.5)
+        .lineWidth(.5)
+        .opacity(.5)
+        .stroke()
+        .opacity(1); // Reset opacity after drawing the line
+    });
+
+    this.x = startX;
+    this.y = rowBottomY; //tableMaxHeightY;
+    this.moveDown();
+
+    return this;
+  }
+}
+
+module.exports = PDFDocumentWithTables;
