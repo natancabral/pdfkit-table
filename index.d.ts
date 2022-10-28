2a16ab3589eedbfd8d8b38ace3b8f651eabaddca
Nicolas FABRE <nicolas.fabre@younited-credit.fr>
2022-10-28 18:48:00 +0200

    Export types and add addBackground function

--- index.d.ts
+++ index.d.ts
@@ -1,99 +1,104 @@
-declare module 'pdfkit-table' 
-{
-	import PDFDocument from 'pdfkit';
+declare module "pdfkit-table" {
+    import PDFDocument from "pdfkit";
 
-	interface Rect {
-		x: number;
-		y: number;
-		width: number;
-		height: number;
-	}
+    export interface Rect {
+        x: number;
+        y: number;
+        width: number;
+        height: number;
+    }
 
-	interface Header {
-		label?: string;
-		property?: string;
-		width?: number;
-		align?: string; //default 'left'
-		valign?: string;
-		headerColor?: string; //default '#BEBEBE'
-		headerOpacity?: number; //default '0.5'
-		headerAlign?: string; //default 'left'
-		columnColor?: string;
-		columnOpacity?: number;
-		renderer?: (
-			value: any,
-			indexColumn?: number,
-			indexRow?: number,
-			row?: number,
-			rectRow?: Rect,
-			rectCell?: Rect
-		) => string;
-	}
+    export interface Header {
+        label?: string;
+        property?: string;
+        width?: number;
+        align?: string; //default 'left'
+        valign?: string;
+        headerColor?: string; //default '#BEBEBE'
+        headerOpacity?: number; //default '0.5'
+        headerAlign?: string; //default 'left'
+        columnColor?: string;
+        columnOpacity?: number;
+        renderer?: (
+            value: any,
+            indexColumn?: number,
+            indexRow?: number,
+            row?: number,
+            rectRow?: Rect,
+            rectCell?: Rect
+        ) => string;
+    }
 
-	interface DataOptions {
-		fontSize: number;
-		fontFamily: string;
-		separation: boolean;
-	}
+    export interface DataOptions {
+        fontSize: number;
+        fontFamily: string;
+        separation: boolean;
+    }
 
-	interface Data {
-		[key: string]: string | { label: string; options?: DataOptions };
-	}
+    export interface Data {
+        [key: string]: string | { label: string; options?: DataOptions };
+    }
 
-	interface Table {
-		title?: string;
-		subtitle?: string;
-		headers?: (string | Header)[];
-		datas?: Data[];
-		rows?: string[][];
-	}
+    export interface Table {
+        title?: string;
+        subtitle?: string;
+        headers?: (string | Header)[];
+        datas?: Data[];
+        rows?: string[][];
+    }
 
-	interface DividerOptions {
-		disabled?: boolean;
-		width?: number;
-		opacity?: number;
-	}
+    export interface DividerOptions {
+        disabled?: boolean;
+        width?: number;
+        opacity?: number;
+    }
 
-	interface Divider {
-		header?: DividerOptions;
-		horizontal?: DividerOptions;
-	}
+    export interface Divider {
+        header?: DividerOptions;
+        horizontal?: DividerOptions;
+    }
 
-	interface Title 
-	{
-		label: string;
-		fontSize?: number;
-		fontFamily?: string;
-		color?: string; 
-	}
+    export interface Title {
+        label: string;
+        fontSize?: number;
+        fontFamily?: string;
+        color?: string;
+    }
 
-	interface Options {
-		title?: string | Title ;
-		subtitle?: string | Title;
-		width?: number;
-		x?: number; //default doc.x
-		y?: number; //default doc.y
-		divider?: Divider;
-		columnsSize?: number[];
-		columnSpacing?: number; //default 5
-		padding?: number[]; 
-		addPage?: boolean; //default false
-		hideHeader?: boolean;
-		minRowHeight?: number;
-		prepareHeader?: () => PDFDocumentWithTables;
-		prepareRow?: (
-			row?: any,
-			indexColumn?: number,
-			indexRow?: number,
-			rectRow?: Rect,
-			rectCell?: Rect
-		) => PDFDocumentWithTables;
-	}
+    export interface Options {
+        title?: string | Title;
+        subtitle?: string | Title;
+        width?: number;
+        x?: number; //default doc.x
+        y?: number; //default doc.y
+        divider?: Divider;
+        columnsSize?: number[];
+        columnSpacing?: number; //default 5
+        padding?: number[];
+        addPage?: boolean; //default false
+        hideHeader?: boolean;
+        minRowHeight?: number;
+        prepareHeader?: () => PDFDocumentWithTables;
+        prepareRow?: (
+            row?: any,
+            indexColumn?: number,
+            indexRow?: number,
+            rectRow?: Rect,
+            rectCell?: Rect
+        ) => PDFDocumentWithTables;
+    }
 
-	class PDFDocumentWithTables extends PDFDocument {
-		public table(table: Table, options?: Options): Promise<void>;
-	}
+    class PDFDocumentWithTables extends PDFDocument {
+        public table(table: Table, options?: Options): Promise<void>;
 
-	// export = PDFDocumentWithTables;
-	export default PDFDocumentWithTables;
+        public addBackground(
+            rect?: Rect,
+            fillColor?: string,
+            fillOpacity: number,
+            callback?: Function
+        );
+    }
+
+    // export = PDFDocumentWithTables;
+    export default PDFDocumentWithTables;
 }
