c9be5524a992f85f6adc59934a5d64fec9629f71
Khang Mai <khang@ulesson.com>
2023-12-11 18:18:48 +0700

    Update pdfkit table

--- index.js
+++ index.js
@@ -657,7 +657,7 @@ class PDFDocumentWithTables extends PDFDocument {
           // For safety, consider 3 rows margin instead of just one
           // if (startY + 2 * rowHeight < maxY) startY = rowBottomY + columnSpacing + rowDistance; // 0.5 is spacing rows
           // else this.emitter.emit('addPage'); //this.addPage();
-          if(options.useSafelyMarginBottom && this.y + safelyMarginBottom + rowHeight >= maxY && !lockAddPage) onFirePageAdded(); // this.emitter.emit('addPage'); //this.addPage();    
+          if (options.useSafelyMarginBottom && rowBottomY + rowHeight + columnSpacing + safelyMarginBottom >= maxY && !lockAddPage)  onFirePageAdded(); // this.emitter.emit('addPage'); //this.addPage();    
 
           // calc position
           startY = rowBottomY + columnSpacing + rowDistance; // 0.5 is spacing rows
